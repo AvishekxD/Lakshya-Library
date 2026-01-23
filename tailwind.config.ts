7bd0efe287031f507505a6fc0dc2a284da9263b6
AvishekxD <abmeena00@gmail.com>
2026-01-23 10:29:00 +0530

    chore: add tailwindcss plugins. chore: removed image.remotePatterns from next.config.ts due to no longer use. chore: added packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,5 +7,12 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: { addUtilities: (utilities: Record<string, Record<string, string>>) => void }) {
+      addUtilities({
+        '.perspective-1000': { perspective: '1000px' },
+        '.backface-hidden': { 'backface-visibility': 'hidden' },
+      })
+    },
+  ],
 }
