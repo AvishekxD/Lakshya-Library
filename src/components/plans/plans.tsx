6784b853366bf7eb1a471115e21556f73db15c25
AvishekxD <abmeena00@gmail.com>
2026-01-31 10:42:00 +0530

    Feat: add Plans sections

---
+++ src/components/plans/plans.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import BookAnimation from "../ui/book";
+import BookAnimation1 from "../ui/book-page-mobile";
+
+export default function Plans () {
+    return (
+        <div className="mx-auto flex flex-row items-center justify-center">
+            
+            <div className="mt-20 hidden md:flex"> 
+                <BookAnimation />
+            </div>
+            <div className="md:hidden">
+                <BookAnimation1 />
+            </div>
+        </div>
+    );
+}
